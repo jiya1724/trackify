cf3170797c19aea0a7636e751c503c9fbf557e71
jiya1724 <jiyatrivedi204@gmail.com>
2024-09-17 20:50:35 +0530

    splash screen

--- frontend/screens/Landing.js
+++ frontend/screens/Landing.js
@@ -1,31 +1,55 @@
 import React, { useEffect, useRef } from 'react';
 import { View, Image, StyleSheet, Animated } from 'react-native';
+import Digi from '../assets/digiIndia.svg';
+
 const Landing = () => {
   const fadeAnim = useRef(new Animated.Value(0)).current; 
+  const scaleAnim = useRef(new Animated.Value(0)).current; // Initial value for scale: 0 (invisible)
+
   useEffect(() => {
+    
     Animated.timing(fadeAnim, {
       toValue: 1, 
       duration: 2000, 
       useNativeDriver: true,
     }).start();
-  }, [fadeAnim]);
+
+    
+    Animated.sequence([
+      Animated.timing(scaleAnim, {
+        toValue: 1.5,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scaleAnim, {
+        toValue: 1, 
+        duration: 1000,
+        useNativeDriver: true,
+      })
+    ]).start();
+  }, [fadeAnim, scaleAnim]);
+
   return (
-    <View className='flex justify-center items-center w-full h-full bg-bg'>
-    <Animated.View style={{ opacity: fadeAnim }}>
-      <Image 
-        source={require('../assets/Logo.png')} 
-        style={styles.logo} 
-      />
-    </Animated.View>
+    <View className='flex flex-col justify-center items-center w-full h-full bg-bg'>
+      
+      <Animated.View style={{ opacity: fadeAnim }}>
+        <Image 
+          source={require('../assets/Logo.png')} 
+          style={styles.logo} 
+        />
+      </Animated.View>
+
+    
+      <Animated.View className='absolute bottom-16' style={{ opacity: fadeAnim, transform: [{ scale: scaleAnim }] }}>
+        <Digi />
+      </Animated.View>
     </View>
-  )
-}
+  );
+};
 
-
-export default Landing
+export default Landing;
 
 const styles = StyleSheet.create({
-  
   logo: {
     width: 100,
     height: 113,
